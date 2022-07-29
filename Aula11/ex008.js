eac9d1aef3323616661741571d2e2b67e24305c2
David <davidroussenq@gmail.com>
2022-07-29 20:49:21 -0300

    aula 11 iniciada

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    if (vel > 60) { //condicao simples
+        console.log(`Voce ultrapassou a velocidade permitida,MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
+
+//aperte F8 para executar com node exec, no vscode
---
+++ Aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'BRASIL'
+if (pais == 'brasil') {
+    console.log(`Vivendo em ${pais}, você é brasileiro`)    
+} else { //condição composta
+    console.log(`Vivendo em ${pais}, você é estrangeiro`)
+}
