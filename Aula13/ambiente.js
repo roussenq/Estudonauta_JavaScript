1d62eeae96c0457a06bc81706d7a810fc37d86a1
David <davidroussenq@gmail.com>
2022-08-02 21:31:07 -0300

    aula 12
    inicio de modelo de exercício

---
+++ Aula13/ambiente.js
@@ -0,0 +1,6 @@
+var c = 0
+do {
+    console.log(`lá tudo bem?, contador n° ${++c}!`)
+} while (c < 6)
+
+
---
+++ Aula14/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+do {
+    console.log(c)
+    c++
+} while (c <= 10)
+
+for (c; c <= 15; c++) {
+    console.log(c)
+}
