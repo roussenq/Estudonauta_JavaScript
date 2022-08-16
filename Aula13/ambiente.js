045ad3fa4062eff505e73e74291e2fb1b9beb2f7
David <davidroussenq@gmail.com>
2022-08-16 00:04:01 -0300

    aula 14 - debug

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -1,6 +1,6 @@
 var c = 0
 do {
-    console.log(`lá tudo bem?, contador n° ${++c}!`)
+    console.log(`Olá tudo bem?, contador n° ${++c}!`)
 } while (c < 6)
 
 
---
+++ Aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log("Vai começar!");
+for (var c = 0; c <= 6; c++) {
+    console.log(`Contador = ${c}`)
+}
+console.log('Acabou')
